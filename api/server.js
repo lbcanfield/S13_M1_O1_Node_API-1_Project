76c690122bfab2c089d7b4847540c6a8ecabeabc
lbcanfield <lewie.b.canfield@gmail.com>
2022-12-22 17:30:38 -0600

    package.json - installed 'nodemon' to keep the server going through file changes - installed 'express' - created a script to run nodemon on index.js
    index.js
    - invoked the server module to listen on the provided port and report back to the console
    
    /api/server.js
    - express requires the express module
    - server is assigned the express function
    - exporting server rather than an empty object

--- api/server.js
+++ api/server.js
@@ -1,3 +1,12 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();     //Instance of 'express' application
+server.use(express.json());   //Global Middleware
+
+
+
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
