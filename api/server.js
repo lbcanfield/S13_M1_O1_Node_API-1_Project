36699dd76e74f953d7fca54a1a6f2f955f364adc
lbcanfield <lewie.b.canfield@gmail.com>
2022-12-22 18:16:14 -0600

    /api/server.js created entpoint (post) to add new users

--- api/server.js
+++ api/server.js
@@ -16,7 +16,6 @@ server.get('/sanity-check', (request, response) => {
 /////////////////////SETTING UP ENDPOINTS FOR MVP
 //Endpoint: get (Returns an array of users)
 server.get('/api/users', async (request, response) => {
-     //finish
      try {
           const users = await Users.find();
           // console.log(users) //users successfully logged to the console
@@ -28,8 +27,28 @@ server.get('/api/users', async (request, response) => {
 })
 
 //Endpoint: post (Creates a user using the information sent inside the 'request body')
-server.post('/api/users', (request, response) => {
+server.post('/api/users', async (request, response) => {
      //finish
+     try {
+          const { name, bio } = request.body;
+          if (!name || !bio) {     //for new users, both fields are required
+               response.status(422).json({   //422 - Unprocessable Entry
+                    message: 'Users need a name and a biography'
+               })
+          }
+          else {
+               const newUser = await Users.insert({ name, bio });
+               response.status(201).json({ //201 - Created //Success in postman
+                    message: `User: ${name} successfully created!`,
+                    data: newUser
+               })
+          }
+     }
+     catch (error) {
+          response.status(500).json({
+               message: `Error creating user: ${error.message}`,
+          })
+     }
 })
 
 //Endpoint: get (Returns a specific user based on the id value)
