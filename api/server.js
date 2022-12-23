c3f9889999f125d25b6bd91da4fe0df4df8a5bbd
lbcanfield <lewie.b.canfield@gmail.com>
2022-12-22 19:47:22 -0600

    Watched the video to pass the MVP test.  Wondering why it wasn't taught the way it needed to be passed.

--- api/server.js
+++ api/server.js
@@ -31,18 +31,22 @@ server.get('/api/users', async (request, response) => {
 //Endpoint: post (Creates a user using the information sent inside the 'request body')
 server.post('/api/users', async (request, response) => {
      try {
-          const { name, bio } = request.body;
-          if (!name || !bio) {     //for new users, both fields are required
+          const user = request.body;
+          if (!user.name || !user.bio) {     //for new users, both fields are required
                response.status(400).json({
                     message: "Please provide name and bio for the user"
                })
           }
           else {
-               const newUser = await Users.insert({ name, bio });
-               response.status(201).json({ //201 - Created //Success in postman
-                    message: newUser,
-                    data: newUser
-               })
+               // const newUser = await Users.insert(user);
+               // response.status(201).json({ //201 - Created //Success in postman
+               //      message: newUser,
+               //      data: newUser
+               // })
+               Users.insert(user)
+                    .then(createdUser => {
+                         response.status(201).json(createdUser)
+                    })
           }
      }
      catch (error) {
@@ -77,24 +81,43 @@ server.get('/api/users/:id', async (request, response) => {
 //Endpoint: delete (Removes a specific user based on the id value)
 server.delete('/api/users/:id', async (request, response) => {
      //finish
+     // try {
+     //      const { id } = request.params;
+     //      const removeUser = await Users.remove(id);
+     //      if (!removeUser) {
+     //           response.status(404).json({
+     //                message: "The user with the specified ID does not exist"
+     //           })
+     //      }
+     //      else {
+     //           response.status(202).json({ //202 - Accepted
+     //                message: "User removed from database",
+     //                data: removeUser
+     //           })
+     //      }
+     // }
+     // catch (error) {
+     //      response.status(500).json({
+     //           message: "The user could not be removed"
+     //      })
+     // }
+     /////////////Based off not what I learned but the video... why wasn't it taught this way?
      try {
-          const { id } = request.params;
-          const removeUser = await Users.remove(id);
-          if (!removeUser) {
+          const possibleUser = await Users.findById(request.params.id)
+          if (!possibleUser) {
                response.status(404).json({
                     message: "The user with the specified ID does not exist"
                })
           }
           else {
-               response.status(202).json({ //202 - Accepted
-                    message: "User removed from database",
-                    data: removeUser
-               })
+               const deletedUser = await Users.remove(possibleUser.id)
+               response.status(200).json(deletedUser)
           }
      }
      catch (error) {
           response.status(500).json({
-               message: "The user could not be removed",
+               err: err.message,
+               stack: err.stack
           })
      }
 })
@@ -102,32 +125,58 @@ server.delete('/api/users/:id', async (request, response) => {
 //Endpoint: put (updates a specific user based on the id value)
 server.put('/api/users/:id', async (request, response) => {
      //finish
+     // try {
+     //      const { id } = request.params;
+     //      const { name, bio } = request.body;
+     //      if (!name || !bio) {
+     //           response.status(400).json({
+     //                message: "Please provide name and bio for the user"
+     //           })
+     //      }
+     //      else {
+     //           const updatedUser = await Users.update(id, { name, bio })
+     //           if (!updatedUser) {
+     //                response.status(404).json({
+     //                     message: "The user with the specified ID does not exist"
+     //                })
+     //                console.log(updatedUser);
+     //           }
+     //           else {
+     //                response.status(200).json({
+     //                     updatedUser
+     //                })
+     //           }
+     //      }
+     // }
+     // catch (error) {
+     //      response.status(500).json({
+     //           message: "The user information could not be modified",
+     //      })
+     // }
      try {
-          const { id } = request.params;
-          const { name, bio } = request.body;
-          if (!name || !bio) {
-               response.status(400).json({
-                    message: "Please provide name and bio for the user"
+          const possibleUser = await Users.findById(request.params.id);
+          if (!possibleUser) {
+               response.status(404).json({
+                    message: "The user with the specified ID does not exist"
                })
           }
           else {
-               const updateUser = await Users.update(id, { name, bio })
-               if (!updateUser) {
-                    response.status(404).json({
-                         message: "The user with the specified ID does not exist"
+               if (!request.body.name || !request.body.bio) {
+                    response.status(400).json({
+                         message: "Please provide name and bio for the user"
                     })
                }
                else {
-                    response.status(200).json({
-                         message: "User updated successfully",
-                         data: updateUser
-                    })
+                    const updatedUser = await Users.update(request.params.id, request.body);
+                    response.status(200).json(updatedUser)
                }
           }
      }
-     catch (error) {
+     catch (err) {
           response.status(500).json({
                message: "The user information could not be modified",
+               err: err.message,
+               stack: err.stack
           })
      }
 })
